c8b7f0e5d294f1b72d85e1772c2d2d8da4336cb1
Matias Seguel <matias.seguel.estay@gmail.com>
2016-11-14 14:57:36 -0300

    fixed some issues

--- wp-content/themes/gofactoring/page-beneficios.php
+++ wp-content/themes/gofactoring/page-beneficios.php
@@ -1,36 +1,36 @@
 <?php get_header() ?>
 
 <div class="page-title">
-	<div class="background-overlay padd-tb-150">
-	</div>
-</div>
-
-
-<section class="body-content">
-	<div id="about" class="section-content">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-7 col-sm-12">
-					<div class="section-title">
-						<h2 class="text-left"><span class="font-blue">Beneficios</span></h2>
-					</div>
-					<ul>
-						<li>Acceso a una nueva fuente de financiamiento que complementa las líneas de crédito tradicionales.</li>
-						<li>Mejora la liquidez de las empresas.</li>
-						<li>Reduce los tiempos de cobro y riesgo de no pago de las cuentas por cobrar.</li>
-						<li>Externaliza la gestión de cobro.</li>
-						<li>Mejora los índices de endeudamiento de la empresa.</li>
-						<li>Permite negociar descuentos con sus proveedores.</li>
-						<li>Posibilita la obtención de descuentos por pronto pago.</li>
-						<li>No se publica la deuda en el Dicom.</li>
-					</ul>
-				</div>
-				<div class="col-md-5 col-sm-12">
-					<img src="<?php bloginfo('template_url') ?>/images/section/about.png" alt="Nuestra Empresa" class="img-responsive">
-				</div>
+			<div class="background-overlay padd-tb-150">
 			</div>
 		</div>
-	</div>
-</section>
+
+
+		<section class="body-content">
+			<div id="about" class="section-content">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-7 col-sm-12">
+							<div class="section-title">
+								<h2 class="text-left"><span class="font-blue">Beneficios</span></h2>
+							</div>
+							<ul class="list-awesome">
+								<li>Acceso a una nueva fuente de financiamiento que complementa las líneas de crédito tradicionales.</li>
+								<li>Mejora la liquidez de las empresas.</li>
+								<li>Reduce los tiempos de cobro y riesgo de no pago de las cuentas por cobrar.</li>
+								<li>Externaliza la gestión de cobro.</li>
+								<li>Mejora los índices de endeudamiento de la empresa.</li>
+								<li>Permite negociar descuentos con sus proveedores.</li>
+								<li>Posibilita la obtención de descuentos por pronto pago.</li>
+								<li>No se publica la deuda en el Dicom.</li>
+							</ul>
+						</div>
+						<div class="col-md-5 col-sm-12">
+							<img src="<?php bloginfo('template_url') ?>/images/section/beneficios.png" alt="Nuestra Empresa" class="img-responsive">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
 <?php get_footer() ?>
